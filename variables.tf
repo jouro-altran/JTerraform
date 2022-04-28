308cad72bf731d33ef29414c44c1f2514ec4f6a6
jouro-altran <94450828+jouro-altran@users.noreply.github.com>
2022-04-28 11:35:19 +0100

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "aws_access_key" {
+  type      = string
+  default   = "AKIAQ6IWLD3PVFQIQJPQ"
+  sensitive = true
+}
+
+variable "aws_secret_key" {
+  type      = string
+  default   = "zQwiinkEWiq86lZq1kgjESHhwov1u+V5kZQJF43W"
+  sensitive = true
+}
